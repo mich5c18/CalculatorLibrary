4785912f254b4f7370ac4107b5e5b1bbd0739677
michaelserupmj <michaelserupmj@gmail.com>
2018-10-08 13:11:52 +0200

    Add project files.

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public static double Add(double v1, double v2)
+        {
+            double add = 0;
+            add = v1 + v2;
+
+            return add;
+        }
+
+        public static double Subtract(double v1, double v2)
+        {
+            double subtract = 0;
+            subtract = v1 - v2;
+
+            return subtract;
+        }
+
+        public static double Multiply(double v1, double v2)
+        {
+            double multiply = 0;
+            multiply = v1 * v2;
+
+            return multiply;
+        }
+
+        public static double Divide(double v1, double v2)
+        {
+            double divide = 0;
+            divide = v1 / v2;
+
+            return divide;
+        }
+
+        public static double Sum(double[] numbers)
+        {
+                double sum = 0;
+
+                for (int i = 0; i < numbers.Length; i = i + 1)
+                {
+                    sum = sum + numbers[i];
+                }
+
+                return sum;
+        }
+
+        public static double Minimum(double[] numbers)
+        {
+            double min = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i = i + 1)
+            {
+                if (min > numbers[i])
+                {
+                    min = numbers[i];
+                }
+            }
+
+            return min;
+        }
+
+        public static double Maximum(double[] numbers)
+        {
+            double max = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i = i + 1)
+            {
+                if (max < numbers[i])
+                {
+                    max = numbers[i];
+                }
+            }
+
+            return max;
+        }
+
+        public static double Average(double[] numbers)
+        {
+            double average = 0;
+
+            for (int i = 0; i < numbers.Length; i = i + 1)
+            {
+                average = average + numbers[i];
+            }
+
+            return average / numbers.Length;
+        }
+    }
+}
---
+++ CalculatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("956d19f1-1a20-4794-bc15-fa6733e58b0b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
